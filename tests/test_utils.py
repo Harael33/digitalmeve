af11dfe2f0f91d412b06fbfda12031b8f9bb84af
BACOUL <102251510+BACOUL@users.noreply.github.com>
2025-08-27 12:52:57 +0200

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,2 +1,9 @@
-def test_utils_runs():
-    assert True
+# tests/test_utils.py
+
+from src.utils import add
+
+def test_add_simple():
+    assert add(2, 3) == 5
+
+def test_add_negative():
+    assert add(-1, -1) == -2
