a7c26397c89b9de7452719e5c8dac57ae24c16bf
BACOUL <102251510+BACOUL@users.noreply.github.com>
2025-08-28 09:36:57 +0200

    Update __init__.py

--- src/digitalmeve/__init__.py
+++ src/digitalmeve/__init__.py
@@ -1,16 +1,1 @@
-"""
-DigitalMeve package init.
-Expose la version et les APIs publiques de base.
-"""
-
-__version__ = "0.1.4"   # aligne ceci avec ta prochaine release/tag
-
-from .generator import generate_meve, save_meve
-from .verifier  import verify_meve
-
-__all__ = [
-    "__version__",
-    "generate_meve",
-    "save_meve",
-    "verify_meve",
-]
+__version__ = "0.1.4"
