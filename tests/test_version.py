90a677a184dc7ac90af31cc2129ed0114fd8aec7
BACOUL <102251510+BACOUL@users.noreply.github.com>
2025-08-29 13:14:14 +0200

    Update test_version.py

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,6 +1,3 @@
-import digitalmeve
-
-
 def test_version():
-    # Keep in sync with src/digitalmeve/__init__.py
+    # Keep this in sync with src/digitalmeve/__init__.py
     assert digitalmeve.__version__ == "1.6.0"
