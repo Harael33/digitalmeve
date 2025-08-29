ef5fff742c695b0a0e9e7b73fd19044ce1ba5de7
BACOUL <102251510+BACOUL@users.noreply.github.com>
2025-08-29 08:52:07 +0200

    Update test_version.py

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,5 +1,6 @@
---- a/tests/test_version.py
-+++ b/tests/test_version.py
-@@
--    assert digitalmeve.__version__ == "0.1.5"
-+    assert digitalmeve.__version__ == "0.1.6"
+import digitalmeve
+
+
+def test_version():
+    # Keep this in sync with src/digitalmeve/__init__.py
+    assert digitalmeve.__version__ == "0.1.5"
