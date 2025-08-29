fcb5abe8c2be2644416b480fc75b54f683e93f00
BACOUL <102251510+BACOUL@users.noreply.github.com>
2025-08-29 21:20:52 +0200

    Update core.py

--- src/digitalmeve/core.py
+++ src/digitalmeve/core.py
@@ -1,7 +1,3 @@
-# au lieu de tout sur une ligne trop longue
-return {"filename": filename, "size": size, "mime": mime, "sha256": sha256}
-
-# si besoin, casse comme ça
 return {
     "filename": filename,
     "size": size,
