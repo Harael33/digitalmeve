9acaf0de2c98e3c69ba550c9eed879ec9a3554a8
BACOUL <102251510+BACOUL@users.noreply.github.com>
2025-08-30 10:43:37 +0200

    Update __init__.py

--- src/digitalmeve/__init__.py
+++ src/digitalmeve/__init__.py
@@ -1,10 +1,7 @@
 """
 DigitalMeve - API publique
 """
-
 from .core import generate_meve, verify_meve  # noqa: F401
 
 __all__ = ["generate_meve", "verify_meve"]
-
-# Pense à tenir sync avec tests/test_version.py
-__version__ = "1.6.0"
+__version__ = "1.6.1"
